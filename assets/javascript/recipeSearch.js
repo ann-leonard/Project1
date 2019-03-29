4b21e91c87de7ba6c964b4827aee58ec1832fab1
ann-leonard <aleo0779@gmail.com>
2019-03-29 19:59:46 -0400

    recipeSearch and Search result code

--- assets/javascript/recipeSearch.js
+++ assets/javascript/recipeSearch.js
@@ -1,95 +1,136 @@
-//variables to store user choice
-var areaFilter = '';
-var categoryFilter = '';
-//keeps track of whether a button was selected or not
-var areaChosen = false;
-var categoryChosen= false;
 
-$(document.body).on("click", ".filter", function(){
-    var clicked = $(this).attr("clicked")
-    var group = $(this).offsetParent()
-    if (group.attr("data-choice")=== "true"){
-       $(this).attr("data-selected", "true")
-        
-    }
-    if (group.attr("data-choice") === "false"){
-        $(this).attr("data-selected", "false")
-        if (clicked === "off"){
-            $(this).attr("clicked", "on");
-            $(group).attr("data-choice", "true")
-            // ADD THE X
-            // Create a Div to hold the x
-            var xdiv = $("<img class='ml-2 selectedButton' src=assets/images/x.png>")
-            // append xdiv to html
-            $(this).append(xdiv)
+    // Variables to store the users choice
+   var areaChosen 
+   var categoryChosen
+
+    //Variables to keep track of clicked buttons
+
+    var area = false
+    var category = false
     
-            //CHANGE THE BUTTON'S COLOR
-            //Remove current color
-            $(this).removeClass("secondarycolor buttoncolor pr-3")
-            //Add new color
-            $(this).addClass("clickedcolors")
-    
-            //add to dr
+    //Click function for ctrecipebuttons
+   $(".arrecipebttn").on("click", function () {
+            //var to check the value of clicked
+                var clicked = $(this).attr("clicked")
+               // console.log(clicked)
+                if (area === false){
 
-            if ($(this).hasClass("arrecpiebttn")){
-                areaChosen = true;
-                areaFilter = $(this).attr("value")
+                    if (clicked === "off"){
+                        // CHANGE CLICKED FROM OFF TO ON
+                        $(this).attr("clicked", "on");
 
-            }
-            else{
-                categoryChosen = true;
-                categoryFilter = $(this).attr("value")
-            }
+                        // ADD THE X
+                        // Create a Div to hold the x
+                        var xdiv = $("<img class='ml-2 xarbutton' src=../assets/images/x.png>")
+                        // append xdiv to html
+                        $(this).append(xdiv)
 
-        }
-        
-    }
-    else{
-        
-            //CHANGE CLIKED FROM ON TO OFF
-            $(this).attr("clicked", "off");
-            if ($(this).attr("data-selected"==="true")){
-                
+                        //CHANGE THE BUTTON'S COLOR
+                        //Remove current color
+                        $(this).removeClass("secondarycolor buttoncolor pr-3")
+                        //Add new color
+                        $(this).addClass("clickedcolors")
+
+                        area = true;
+
+                        //Add the choice to the variable
+                        areaChosen = $(this).attr("value")
+                        console.log(areaChosen)
+                    }
+                }
+                else{
+                    if (clicked === "on"){
+                        //CHANGE CLIKED FROM ON TO OFF
+                        $(this).attr("clicked", "off");
+
+                        //REMOVE THE X
+                        $(".xarbutton").remove()
+
+                        //CHANGE COLORS BACK
+                        //remove current colors
+                        $(this).removeClass("clickedcolors")
+                        //add original colors
+                        $(this).addClass("secondarycolor buttoncolor pr-3")
+
+                        //remove from dr
+                        area = false
+
+                        //remove choice from variable
+                        areaChosen = ""
+                        console.log(areaChosen)
+                    }
+                }
+                userSearchInfo();
+
+            });  
             
-            //REMOVE THE X
-           $(".selectedButton").remove()
-           // $(".xctbutton").remove()
+    //Click function for ctrecipebuttons
+    $(".ctrecipebttn").on("click", function () {
+            //var to check the value of clicked
+                var clicked = $(this).attr("clicked")
+               // console.log(clicked)
+                if (category === false){
 
-            //CHANGE COLORS BACK
-            //remove current colors
-            $(this).removeClass("clickedcolors")
-            //add original colors
-            $(this).addClass("secondarycolor buttoncolor pr-3")
+                    if (clicked === "off"){
+                        // CHANGE CLICKED FROM OFF TO ON
+                        $(this).attr("clicked", "on");
 
-            //make choice false
-            $(group).attr("data-choice", "false")
+                        // ADD THE X
+                        // Create a Div to hold the x
+                        var xdiv = $("<img class='ml-2 xctbutton' src=../assets/images/x.png>")
+                        // append xdiv to html
+                        $(this).append(xdiv)
 
-            areaFilter = '';
-            categoryFilter = '';
-            }
-             
-}
-    
-})
+                        //CHANGE THE BUTTON'S COLOR
+                        //Remove current color
+                        $(this).removeClass("secondarycolor buttoncolor pr-3")
+                        //Add new color
+                        $(this).addClass("clickedcolors")
 
-function getResponse(queryURL){
-    $.ajax({
-        url: queryURL
-        ,
-        method: "GET"
-      }).then(function(response) {
-          console.log(response)
-    //iterates through response and checks for filters
-        console.log(queryURL)
-        console.log(response.meals[0].strArea)
-      });
-}
+                        //add to dr
+                        category = true;
 
+                        //Add the choice to the variable
+                        categoryChosen = $(this).attr("value")
+                        
+                    }
+                }
+                else{
+                    if (clicked === "on"){
+                        //CHANGE CLIKED FROM ON TO OFF
+                        $(this).attr("clicked", "off");
 
-$("#submitbttn").on("click",function(){
-    var userInput = $(".userInput").val();
-    var queryURL = "https://www.themealdb.com/api/json/v1/1/filter.php?i=" + userInput
-    console.log(userInput)
-    getResponse(queryURL)
-    $(".userInput").val("")
-})
+                        //REMOVE THE X
+                        $(".xctbutton").remove()
+
+                        //CHANGE COLORS BACK
+                        //remove current colors
+                        $(this).removeClass("clickedcolors")
+                        //add original colors
+                        $(this).addClass("secondarycolor buttoncolor pr-3")
+
+                        //remove from dr
+                        category = false;
+
+                        //remove choice from variable
+                        categoryChosen = ""
+                    }
+                }
+                userSearchInfo()
+
+            });
+        
+        
+           
+
+ function userSearchInfo(){
+    var userInput = $("#userInput").val();
+    $("#submitURL").attr("href", `../html/SearchResult.html?Ingredient=${userInput}&Area=${areaChosen}&Category=${categoryChosen}`)
+   
+
+        
+       // getResponse(queryURL)
+      //  $("#userInput").val("")
+     //   return userInput, queryURL
+    }
+
