4d3f512d8dca604b0c1abf0fde53344b6b6e133f
Ann Olivia Leonard <46509039+ann-leonard@users.noreply.github.com>
2019-04-01 18:13:25 -0400

    Update data.js

--- assets/javascript/data.js
+++ assets/javascript/data.js
@@ -145,63 +145,54 @@ $(document).ready(function () {
         });
 
     function getUrlParameter(name) {
-   name = name.replace(/[\[]/, ‘\\[‘).replace(/[\]]/, ‘\\]‘);
-   var regex = new RegExp(‘[\\?&]’ + name + ‘=([^&#]*)‘);
-   var results = regex.exec(location.search);
-   return results === null ? ‘’ : decodeURIComponent(results[1].replace(/\+/g, ' ’));
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
 $(document).ready(function(){
-   var queryURL = “https://www.themealdb.com/api/json/v1/1/lookup.php?i=” + getUrlParameter(“Id”)
-   getResponse(queryURL)
+    var queryURL = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + getUrlParameter("Id") 
+    getResponse(queryURL)
+
+
 })
+
 function getResponse(queryURL){
-   $.ajax({
-       url: queryURL
-       ,
-       method: “GET”
-     }).then(function(response) {
-       console.log(queryURL)
-       console.log(response)
-       var meal = response.meals[0]
-       var recipeImg = response.strMealThumb
-       var recipeName = response.meals[0].strMeal
-       console.log(response.meals[0].strMeal)
-       $(“#recipeName”).append(recipeName)
-       var ingredientbox = $(“<button class=‘buttoncolor secondarycolor roundedcorners px-3 py-1 border-0 m-1’>” +  + “</button>“)
-       console.log(“ingredientbox: ” + ingredientbox)
-       var recipebox = $(“<div  class=‘mt-2 mb-4 buttoncolor roundedcorners px-3 py-2 tittlefont text-muted’>“)
-       recipebox.append(`<div>${meal.strInstructions.split(",")}`)
-       console.log(recipebox)
-       console.log(recipebox.html())
-       var Instructions = meal.strInstructions
-       console.log(Instructions.split(“.”))
-       var InstructionsArray = Instructions.split(“.”)
-       console.log(InstructionsArray)
+    $.ajax({
+        url: queryURL
+        ,
+        method: "GET" 
+      }).then(function(response) {
+        console.log(queryURL)
+        console.log(response)
+        var meal = response.meals[0]
+        var recipeImg = response.strMealThumb
+        var recipeName = response.meals[0].strMeal
+        console.log(response.meals[0].strMeal)
+        $("#recipeName").append(recipeName)
+        var ingredientbox = $("<button class='buttoncolor secondarycolor roundedcorners px-3 py-1 border-0 m-1'>" +  + "</button>")
+        var recipebox = $("<div  class='my-4'>")
+        recipebox.text(meal.strInstructions.split(","))
+        console.log(recipebox.html())
+        //var Instructions = meal.strInstructions
+        //console.log(Instructions.split("."))
+        var ingredients = []
 
-       var ingredients = []
-       var measure = []
-       for (i=1;i<20;i++){
-           if (meal[“strIngredient” + i]){
-               ingredients.push(meal[“strIngredient” + i])
-           }
-       }
-       for (i=1;i<15;i++){
-           if (meal[“strMeasure” + i]){
-               measure.push(meal[“strMeasure” + i])
-           }
-       }
+        for (i=1;i<20;i++){
+            if (meal["strIngredient" + i]){
+                ingredients.push(meal["strIngredient" + i])
+            }
+        }
+        console.log (ingredients)
         for (i=0;i<ingredients.length;i++){
-           $(“#ingredientsList”).append(`<button id="ing" class=" buttonfont secondarycolor buttoncolor border-0 roundedcorners px-3 mt-2 mx-2 ">${measure[i]}<label class="text-muted mb-0 ml-2">${ingredients[i]}</label></button>`)
-       }
-       // $(“#ing”).addClass(“font-weight-bold”)
-       $(“#recipeList”).append(recipebox)
-       $(“#recipeImage”).attr(“src”,meal.strMealThumb)
-       $(“.finishedBtn”).on(“click”,function(){
-       $(“#finishedLink”).attr(“href”,`../html/Congratulations.html?name=${meal.strMeal}`)
-       })
-   })
+            $("#ingredientsList").append(ingredients[i]+"<br>")
+        }
+        $("#recipeList").append(recipebox)
+        $("#recipeImage").attr("src",meal.strMealThumb)
+        $("#finishedBtn").on("click",function(){
+            $("#finishedLink").attr("href",`../html/Congratulations.html?name=${meal.strMeal}`)
+        })
 
-}
                 // var recipeImage = $("#recipeImage");
                 var rName = recipeName;
                 console.log(rName);
