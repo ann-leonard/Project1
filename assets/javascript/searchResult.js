ea5c886d4be3cb885a49302aef72a87595afeb38
Pavel Zapolski <pavelzapolski@Pavels-MacBook-Pro.local>
2019-04-01 17:51:34 -0400

    hopeful

--- assets/javascript/searchResult.js
+++ assets/javascript/searchResult.js
@@ -70,7 +70,7 @@ function getDetails(id_url){
                     //Append the Save and Cook Buttons
                     var buttonsDiv = $("<div class='my-1 d-flex justify-content-around'></div>")
 
-                    buttonsDiv.append("<button class='border-0 bg-light secondarycolor muli buttonfont roundedcorners'>SAVE</button>") 
+                    buttonsDiv.append("<button id='saveIt' class='border-0 bg-light secondarycolor muli buttonfont roundedcorners'>SAVE</button>") 
 
                     buttonsDiv.append(`<a id='cookBtn'  href='../html/recipe.html?Id=${secondResponse.idMeal}' <button class='border-0 bg-light secondarycolor muli buttonfont blueline roundedcorners'>COOK</button></a>`)
                     
